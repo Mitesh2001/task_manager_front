a79d95e3a78cae44fd6179e31ddef83d4383265d
mitesh-arsenaltech <mitesh.ladva@arsenaltech.com>
2024-04-18 14:36:31 +0530

    Installed tailwind css and desing login and registration page

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -1,5 +1,6 @@
 import { Outlet, Route, Routes } from "react-router-dom"
 import Login from "./Login";
+import Registration from "./Registration";
 
 const AuthLayout = () => {
     return (
@@ -10,7 +11,8 @@ const AuthLayout = () => {
 const AuthPage = () => (
     <Routes>
         <Route element={<AuthLayout />}>
-            <Route path='login' element={<Login />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/registration' element={<Registration />} />
             <Route index element={<Login />} />
         </Route>
     </Routes>
